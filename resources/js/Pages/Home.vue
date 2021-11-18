0fb8ff77e6919084b477f7979666c4373d3c9e07
yasmine.marguero <yasmine.margueron@he-arc.ch>
2021-11-18 16:26:50 +0100

    Basic home page

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <Head title="Home" />
+
+        <div>
+            <h1>Home page</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
+import { Head } from '@inertiajs/inertia-vue3'
+
+export default {
+  components: {
+    BreezeAuthenticatedLayout,
+    Head,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
